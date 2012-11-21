3c243e1fb808b5e7c1be370272c95f57abc2d120
Martin Gracik <martin@gracik.me>
2012-11-21 17:25:23 +0100

    Add snippets
    bunch, flatten, partition, unique

---
+++ bunch.py
@@ -0,0 +1,49 @@
+class Bunch(dict):
+    '''
+    >>> arrows = Bunch(left='A', right='D')
+    >>> 'left' in arrows and 'right' in arrows
+    True
+    >>> arrows.left, arrows.right
+    ('A', 'D')
+    >>> print sorted(arrows.items())
+    [('left', 'A'), ('right', 'D')]
+    >>> 'up' in arrows
+    False
+    >>> arrows.up = 'W'
+    >>> 'up' in arrows
+    True
+    >>> print sorted(arrows.items())
+    [('left', 'A'), ('right', 'D'), ('up', 'W')]
+    >>> arrows.down
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Bunch' object has no attribute 'down'
+    >>> arrows['down']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'down'
+    >>> arrows['down'] = 'S'
+    >>> for key in sorted(arrows):
+    ...     key, arrows[key]
+    ('down', 'S')
+    ('left', 'A')
+    ('right', 'D')
+    ('up', 'W')
+    >>> del arrows.up
+    >>> 'up' in arrows
+    False
+    >>> del arrows['down']
+    >>> 'down' in arrows
+    False
+    >>> print sorted(arrows.items())
+    [('left', 'A'), ('right', 'D')]
+    '''
+
+    def __init__(self, *args, **kwds):
+        super(Bunch, self).__init__(*args, **kwds)
+        self.__dict__ = self
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
---
+++ snippets.py
@@ -0,0 +1,65 @@
+import collections
+import itertools
+
+
+def flatten(seq):
+    '''
+    >>> list(flatten([[[0], 1, 2], [3, 4], [5, [6, 7, [8]]], 9]))
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    >>> list(flatten(['aa', ['bb', 'cc'], [['dd', 'ee'], 'ff']]))
+    ['aa', 'bb', 'cc', 'dd', 'ee', 'ff']
+    '''
+
+    for item in seq:
+        if isinstance(item, collections.Iterable) and not isinstance(item, str):
+            for subitem in flatten(item):
+                yield subitem
+        else:
+            yield item
+
+
+def partition(seq, func=bool, func_range=(True, False)):
+    '''
+    >>> map(list, partition(range(10), func=lambda x: x % 2 == 0))
+    [[0, 2, 4, 6, 8], [1, 3, 5, 7, 9]]
+    >>> map(list, partition(range(10), func=lambda x: x % 3, func_range=(0, 1, 2)))
+    [[0, 3, 6, 9], [1, 4, 7], [2, 5, 8]]
+    '''
+
+    buffers = {x: collections.deque() for x in func_range}
+
+    def values(x, seq=iter(seq)):
+        while True:
+            while not buffers[x]:
+                item = seq.next()
+                buffers[func(item)].append(item)
+            yield buffers[x].popleft()
+
+    return tuple(values(x) for x in func_range)
+
+
+def unique(seq, key=None):
+    '''
+    >>> list(unique('ABBA'))
+    ['A', 'B']
+    >>> list(unique('ABBAabba'))
+    ['A', 'B', 'a', 'b']
+    >>> list(unique('ABBAabba', key=str.lower))
+    ['A', 'B']
+    '''
+
+    seen = set()
+    if key:
+        for elem in seq:
+            if key(elem) not in seen:
+                seen.add(key(elem))
+                yield elem
+    else:
+        for elem in itertools.ifilterfalse(seen.__contains__, seq):
+            seen.add(elem)
+            yield elem
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
