1928ae020aa15ac2395a57f72df64f0f612fddc7
Martin Gracik <martin@gracik.me>
2013-07-11 13:49:05 +0200

    Create typecheck.py
    Decorator for type checking of function arguments.

---
+++ typecheck.py
@@ -0,0 +1,26 @@
+from functools import wraps
+import inspect
+
+
+def typechecked(f, *args, **kwargs):
+    argspec = inspect.getfullargspec(f)
+
+    def check(arg, val):
+        annotation = argspec.annotations.get(arg)
+        if annotation and type(val) != annotation:
+            raise TypeError('%s has to be %s' % (arg, annotation))
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        for index, val in enumerate(args):
+            arg = argspec.args[index]
+            check(arg, val)
+        for arg, val in sorted(kwargs.items()):
+            check(arg, val)
+        return f(*args, **kwargs)
+    return wrapper
+
+
+@typechecked
+def add(a: int, b: int):
+    return a + b
